1e4f179c78ee86374f36f6f95f9e93acbcd64e65
FrancoF2002 <elpanqui44@gmail.com>
2022-05-23 20:32:28 -0300

    Component Query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./components/Login";
+import Query from "./components/Query";
 
 function App() {
   return (
     <div className="app d-flex justify-content-center align-items-center bg-dark">
       <Login />
+      <Query />
     </div>
   );
 }
---
+++ src/components/Query.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Query.css"
+
+const Query = () => {
+
+    console.log(inputConsole);
+
+  return (
+    <div>
+        <form action="" >
+            <div class="formulario">
+                <p><label htmlFor="">Nombre y Apellido: <input type="text" /> </label></p>
+                <p><label>Fecha de Consulta: <input type="date"/></label></p>
+                <p>
+                    <label htmlFor="">Seleccione especialidad:</label> 
+                    <select name="" id="">
+                        <option value="">Kinesiologia</option>
+                        <option value="">Pediatria</option>
+                        <option value="">Urgencias</option></select>
+                </p>
+                <p><label> Ingrese su consulta </label></p>
+                <textarea id='inputConsole' className='inputQuery' name="comentarios" rows="10" cols="40" placeholder='Ingrese su consulta aqui...'></textarea>
+                <button>Enviar Consulta</button>
+            </div>
+            
+        </form>
+    </div>
+  );
+};
+
+export default Query
